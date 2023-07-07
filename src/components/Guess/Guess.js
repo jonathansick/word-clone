2379a3cf88b35a0b06eaf83b3ce12892f2a00979
Jonathan Sick <jonathan@jsick.codes>
2023-07-07 11:54:49 -0400

    Add Guess component
    - In the Guess component, render the letters into individual spans
    - GuessResults renders the Guess components, even if the Guess has an
      empty value.

---
+++ src/components/Guess/Guess.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+import { range } from "../../utils";
+
+function Guess({ value }) {
+  return (
+    <p className='guess'>
+      {range(5).map((index) => (
+        <span key={index} className='cell'>
+          {value ? value[index] : ""}
+        </span>
+      ))}
+    </p>
+  );
+}
+
+export default Guess;
--- src/components/GuessResults/GuessResults.js
+++ src/components/GuessResults/GuessResults.js
@@ -1,15 +1,20 @@
 import React from "react";
 
+import { NUM_OF_GUESSES_ALLOWED } from "../../constants";
+import { range } from "../../utils";
+import Guess from "../Guess";
+
 /**
  * Display the guesses that have been submitted.
  */
 function GuessResults({ guesses }) {
   return (
     <div className='guess-results'>
-      {guesses.map((guess) => (
-        <p key={guess.id} className='guess'>
-          {guess.guess}
-        </p>
+      {range(NUM_OF_GUESSES_ALLOWED).map((guessIndex) => (
+        <Guess
+          key={guessIndex}
+          value={guesses.length > guessIndex ? guesses[guessIndex].guess : ""}
+        />
       ))}
     </div>
   );
